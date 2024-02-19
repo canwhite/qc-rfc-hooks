d8a3dff22455509647ced72b0869bc93cf7af078
Zack <752716886@qq.com>
2024-02-19 20:32:12 +0800

    Update usePromise.ts

--- src/hooks/usePromise.ts
+++ src/hooks/usePromise.ts
@@ -1,18 +1,20 @@
-import { useState, useEffect} from "react";
-
+mport { useEffect, useState } from "react";
 const handlePromise = (promise: Promise<unknown>) =>
-  promise.then(data => (data))
-    .catch(error => (error));
-
-const usePromise = (promise: Promise<unknown>) => {
+  promise.then((data) => data).catch((error) => error);
+const usePromise = (promiseFunc: () => Promise<unknown>) => {
   const [result, setResult] = useState<null | unknown>(null);
   useEffect(() => {
     let isMounted = true;
-    handlePromise(promise).then(
-      value => isMounted && setResult(value)
-    );
-    return () => { isMounted = false };
-  }, [promise]);
+    const promise = promiseFunc();
+    handlePromise(promise).then((value) => {
+      if (isMounted) {
+        setResult(value);
+      }
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, [promiseFunc]); 
   return result;
 };
 export default usePromise;
